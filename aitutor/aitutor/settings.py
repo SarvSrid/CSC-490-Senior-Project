2dd6b3e9e023a770a4f8c8cbad299127c8d03f4e
SarvSrid <142545568+SarvSrid@users.noreply.github.com>
2025-02-19 20:40:10 -0500

    made changes to settings

--- aitutor/aitutor/settings.py
+++ aitutor/aitutor/settings.py
@@ -81,27 +81,17 @@ WSGI_APPLICATION = 'aitutor.wsgi.application'
 #     }
 # }
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "mydatabase",
-#         "USER": "mydatabaseuser",
-#         "PASSWORD": "mypassword",
-#         "HOST": "127.0.0.1",
-#         "PORT": "5432",
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'aitutor',
-        'USER': 'postgres',
-        'PASSWORD': 'sharksnow',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mydatabase",
+        "USER": "mydatabaseuser",
+        "PASSWORD": "mypassword",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
