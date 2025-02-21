829191adff69ef92e8792589ec2b7d96e3bfe6b5
SarvSrid <142545568+SarvSrid@users.noreply.github.com>
2025-02-21 13:55:56 -0500

    Starting from scratch after issues arised

--- aitutor/topics/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import TopicList, QuestionList
-
-urlpatterns = [
-    path('topics/', TopicList.as_view()),
-    path('topics/<int:topic_id>/questions/', QuestionList.as_view()),
-]
