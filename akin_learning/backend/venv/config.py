cc44a70354c8d8d83c5d52f11caccc3e0379cebc
SarvSrid <142545568+SarvSrid@users.noreply.github.com>
2025-03-01 23:33:15 -0500

    finshed changed to config and test_db

--- akin_learning/backend/venv/config.py
+++ akin_learning/backend/venv/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://myuser:mypassword@localhost:5432/mydatabase')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:sharksnow@127.0.0.1:5432/akin_learning')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your-secret-key')
 
@@ -9,10 +9,10 @@ class Config:
 # Example:
 # If your PostgreSQL connection details are as follows:
 
-# Username: myuser postgres
-# Password: mypassword sharksnow
+# Username: myuser
+# Password: mypassword
 # Host: localhost 
 # Port: 5432
-# Database: mydatabase akin_learning
+# Database: mydatabase
 # export DATABASE_URL="postgresql://myuser:mypassword@localhost:5432/mydatabase"
 
